4168b57bf08a38287acb1d12487c736b8504dfa4
cyan <cyan.wu@accentrix.com>
2021-04-13 14:41:04 +0800

    update: list

--- src/lib/multiple-search-input.vue
+++ src/lib/multiple-search-input.vue
@@ -152,7 +152,7 @@ export default class MultipleSearchInput<T> extends Vue {
   }
 
   setDefaultTag(value: T[], list: SelectOption<T>[]){
-     if (value && value.length > 0 && this.tags.length === 0) {
+     if (value && value.length > 0 && this.tags.length === 0 && list.length > 0) {
       const options = list
       const textShowArr: string[] = []
       value.forEach(el => {
